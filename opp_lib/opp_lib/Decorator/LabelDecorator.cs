6888f3b6dee86592c519cd39d0846455df44fb4e
Dzytizz <gytisdoksas@gmail.com>
2022-10-23 18:39:54 +0300

    Update LabelDecorator.cs

--- opp_lib/opp_lib/Decorator/LabelDecorator.cs
+++ opp_lib/opp_lib/Decorator/LabelDecorator.cs
@@ -15,12 +15,17 @@ namespace opp_lib.Decorator
         public override OvalPictureBox Display(string teamColor)
         {
             OvalPictureBox pb = Player.Display(teamColor);
+            //pb.Location = new Point((int)Player.XPosition, (int)Player.YPosition);
             Label playerNameLabel = new Label();
             playerNameLabel.AutoSize = true;
-            playerNameLabel.Text = Player.Name;            
+            playerNameLabel.Text = Player.Name;
+            //playerNameLabel.Padding = new Padding(0, 0, 0, 0);
+            //playerNameLabel.Margin = new Padding(0, 0, 0, 0);
             pb.Controls.Add(playerNameLabel);
-            playerNameLabel.Location = new Point((int)Player.XPosition + 25 - (playerNameLabel.Size.Width / 2), (int)Player.YPosition + 25 - (playerNameLabel.Size.Height / 2));
+            playerNameLabel.Location = new Point( 25 - (playerNameLabel.Size.Width / 2),  25 - (playerNameLabel.Size.Height / 2));
+            //playerNameLabel.Location = new Point(25, 25);
             playerNameLabel.BringToFront();
+
             return pb;
         }
     }
