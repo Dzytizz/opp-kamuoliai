7772336f81a215fcfccbf3dd840900d273b94283
ppaulikelis <ppaulikelis@gmail.com>
2022-11-13 23:40:52 +0200

    First test

---
+++ opp-server/opp-server.UnitTests/Classes/Factory/TeamFactoryTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using opp_server.Classes.Factory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opp_server.Classes.Factory.Tests
+{
+    [TestClass()]
+    public class TeamFactoryTests
+    {
+        [TestMethod()]
+        public void GetTeamTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- opp-server/opp-server.UnitTests/Factory/TeamFactoryTests.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace opp_server.UnitTests.Factory
-{
-    [TestClass]
-    public class TeamFactoryTests
-    {
-    }
-}
