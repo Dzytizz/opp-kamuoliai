a64c1cdfdebf5218a07469ce2c3ade475af0c272
ppaulikelis <ppaulikelis@gmail.com>
2022-11-14 12:57:37 +0200

    LevelFactory (1 and 2) tests

---
+++ opp-server/opp-server.UnitTests/Classes/Abstract Factory/Level1FactoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using opp_lib;
+using opp_lib.Fields;
+using opp_lib.Gatess;
+using opp_lib.Obstacles;
+using opp_server.Classes.Abstract_Factory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opp_server.Classes.Abstract_Factory.Tests
+{
+    [TestClass()]
+    public class Level1FactoryTests
+    {
+        [TestMethod()]
+        public void CreateField_CreatesLevel1Field_FieldLevel1()
+        {
+            AbstractLevelFactory levelFactory = new Level1Factory();
+            Field field = levelFactory.CreateField();
+            Assert.IsTrue(field is FieldLevel1);
+        }
+
+        [TestMethod()]
+        public void CreateGates_CreatesLevel1Gates_GatesLevel1()
+        {
+            AbstractLevelFactory levelFactory = new Level1Factory();
+            Gates gates = levelFactory.CreateGates(0, 0);
+            Assert.IsTrue(gates is GatesLevel1);
+        }
+
+        [TestMethod()]
+        public void CreateObstacle_CreatesLevel1Obstacle_ObstacleLevel1()
+        {
+            AbstractLevelFactory levelFactory = new Level1Factory();
+            Obstacle obstacle = levelFactory.CreateObstacle(0, 0);
+            Assert.IsTrue(obstacle is ObstacleLevel1);
+        }
+    }
+}
---
+++ opp-server/opp-server.UnitTests/Classes/Abstract Factory/Level2FactoryTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using opp_lib;
+using opp_lib.Fields;
+using opp_lib.Gatess;
+using opp_lib.Obstacles;
+using opp_server.Classes.Abstract_Factory;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace opp_server.Classes.Abstract_Factory.Tests
+{
+    [TestClass()]
+    public class Level2FactoryTests
+    {
+        [TestMethod()]
+        public void CreateField_CreatesLevel2Field_FieldLevel2()
+        {
+            AbstractLevelFactory levelFactory = new Level2Factory();
+            Field field = levelFactory.CreateField();
+            Assert.IsTrue(field is FieldLevel2);
+        }
+
+        [TestMethod()]
+        public void CreateGates_CreatesLevel2Gates_GatesLevel2()
+        {
+            AbstractLevelFactory levelFactory = new Level2Factory();
+            Gates gates = levelFactory.CreateGates(0, 0);
+            Assert.IsTrue(gates is GatesLevel2);
+        }
+
+        [TestMethod()]
+        public void CreateObstacle_CreatesLevel2Obstacle_ObstacleLevel2()
+        {
+            AbstractLevelFactory levelFactory = new Level2Factory();
+            Obstacle obstacle = levelFactory.CreateObstacle(0, 0);
+            Assert.IsTrue(obstacle is ObstacleLevel2);
+        }
+    }
+}
