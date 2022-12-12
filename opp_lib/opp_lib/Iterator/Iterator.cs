99feb21a9c4384ae095a2a2006dcfb5444938f65
Dzytizz <gytisdoksas@gmail.com>
2022-12-12 23:01:25 +0200

    initial

---
+++ opp_lib/opp_lib/Iterator/Iterator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_lib.Iterator
+{
+    public abstract class Iterator : IEnumerator
+    {
+        public abstract bool MoveNext();
+        public abstract void Reset();
+        public object Current { get; }
+    }
+}
---
+++ opp_lib/opp_lib/Iterator/TeamList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp_lib.Iterator
+{
+    public class TeamList : IteratorAggregate
+    {
+        private List<Team> _collection = new List<Team>();
+
+        public void Add(Team team)
+        {
+            _collection.Add(team);
+        }
+
+        public void Remove(Team team)
+        {
+            _collection.Remove(team);
+        }
+
+        public Team this[int index]
+        {
+            get => _collection[index];
+        }
+        
+        public override IEnumerator GetEnumerator()
+        {
+            return new ListIterator(this);
+        }
+    }
+}
